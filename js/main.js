a3cd5d3786be7bc856eb29536a19b8b7c925e4ff
LucaBrivio183 <l.brivio4@gmail.com>
2023-03-02 17:54:31 +0100

    feat: trying to  add difficulty level

--- js/main.js
+++ js/main.js
@@ -23,21 +23,33 @@ function appendElement(element, parentElement) {
 const grid = document.querySelector('.grid');
 //const for recalling  generate-btn
 const generateButton = document.getElementById('generate-btn');
+const difficulty = document.getElementById('difficulty');
+
 
 generateButton.addEventListener('click', 
     function() {
-        grid.innerHTML = '';
-    for (let i=1; i<=100; i++) {
-        const newElement = createClassElement('li','cell-10');
-        newElement.innerHTML = `${i}`;
-        appendElement(newElement, grid);
-    }
-    let cell = document.querySelectorAll('li');
-    cell.forEach(function (element) {
-        element.addEventListener('click', function() {
-              element.classList.add('clicked-cell');
+        grid.innerHTML = ''; //empty  the grid every click of play button
+        let difficultyValue = difficulty.value;
+        let gridLength = 100;
+        let cellClass = 'cell-10'
+        if (difficultyValue === 2) {
+                gridLength = 81;
+                cellClass = 'cell-9'
+        } else  if (difficultyValue === 3) {
+            gridLength = 49;
+            cellClass = 'cell-7'
+        } 
+     
+        for (let i=1; i<=gridLength; i++) { 
+            const newElement = createClassElement('li',cellClass);
+            newElement.innerHTML = `${i}`;
+            appendElement(newElement, grid);
+        }
+    let cell = document.querySelectorAll('li');//select all cells
+    cell.forEach(function (element) { //create an element for every cell
+        element.addEventListener('click', function() {//waitfor click on cell
+              element.classList.add('clicked-cell'); //add css class at click
           });
       });
 }
 );
-
